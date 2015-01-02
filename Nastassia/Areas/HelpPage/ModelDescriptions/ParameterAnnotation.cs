ff3c600d1f44fc3d1ce7b25dd14c1ea3d7f1a6d4
Eugene Liohenki <prophettime@gmail.com>
2015-01-02 19:15:23 +0300

    Init

---
+++ Nastassia/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nastassia.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ Nastassia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nastassia.WebApiApplication" Language="C#" %>
