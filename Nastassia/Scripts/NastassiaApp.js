68097b05535ff32dd051f7cb5711d9d0b3891dc0
ELIOHENKI <legenkiyes@gmail.com>
2015-02-04 23:11:26 +0300

    Routes

--- Nastassia/Scripts/NastassiaApp.js
+++ Nastassia/Scripts/NastassiaApp.js
@@ -1,3 +1,19 @@
-﻿var NastassiaApp = angular.module('NastassiaApp', []);
+﻿var NastassiaApp = angular.module('NastassiaApp', ['ngRoute']);
 
 NastassiaApp.controller('MainPageController', MainPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo', {
+            templateUrl: 'routesDemo/two'
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        });
+}
+configFunction.$inject = ['$routeProvider'];
+
+NastassiaApp.config(configFunction);
